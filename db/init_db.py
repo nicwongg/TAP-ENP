7d33e820218c9fd3d40c03240b3b601e81c665df
nicwongg <nicwongg@gmail.com>
2022-07-15 01:20:55 +0800

    set up files for heroku deployment
     the commit message #2:

--- db/init_db.py
+++ db/init_db.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('shorturls.db')
+conn = sqlite3.connect('db/shorturls.db')
 
-with open('schema.sql') as f:
+with open('db/schema.sql') as f:
     conn.executescript(f.read())
 conn.execute("INSERT INTO urls (id, shorten_id, original_url) VALUES (100000000, 'RUtE', 'www.example.com')")
 conn.commit()
